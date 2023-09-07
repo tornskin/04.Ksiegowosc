6c09efa199890b08fee6ab96ce8bed2d69501fc8
tornskin <piotr.daniec@gmail.com>
2023-09-07 09:03:58 +0200

    transition to sql database done

--- main.py
+++ main.py
@@ -1,44 +1,39 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 
+# Konfiguracja bazy danych SQLite
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
+db = SQLAlchemy(app)
+
+
+# Definicja modeli
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+
+
+class Transaction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    operation = db.Column(db.String(50), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+
 
 class Manager:
-    actions = {}  # Declaration of the actions dictionary
+    actions = {}
 
     def __init__(self):
         self.saldo = 1000.0
-        self.warehouse = {
-            "bułka": {
-                "ilość": 10,
-                "cena": 2
-            },
-            "bagietka": {
-                "ilość": 5,
-                "cena": 3
-            }
-        }
         self.history = []
         self.initial_message = "Witaj w aplikacji obsługi magazynu. Lista dostępnych operacji:\n" \
-             " 1. Saldo\n 2. Sprzedaż\n 3. Zakup\n 4. Konto\n 5. Lista\n 6. Magazyn\n 7. Przegląd\n 8. Koniec\n"
+                               " 1. Saldo\n 2. Sprzedaż\n 3. Zakup\n 4. Konto\n 5. Lista\n 6. Magazyn\n 7. Przegląd\n 8. Koniec\n"
         self.end_program = False
-
-        self.load_data()
-
-    def save_data(self):
-        with open("data.txt", "w") as file:
-            file.write(str(self.saldo) + "\n")
-            file.write(str(self.warehouse) + "\n")
-            file.write("\n".join(self.history))
-
-    def load_data(self):
-        try:
-            with open("data.txt", "r") as file:
-                self.saldo = float(file.readline().strip())
-                self.warehouse = eval(file.readline().strip())
-                self.history = file.read().splitlines()
-        except FileNotFoundError:
-            print("Plik nie został znaleziony. Tworzenie nowego pliku.")
 
     def execute(self, name):
         if name not in self.actions:
@@ -47,62 +42,84 @@ class Manager:
             self.actions[name](self)
 
     def start(self):
-        self.load_data()
         while not self.end_program:
             print(self.initial_message)
             operation = input("Wybierz operację wprowadzając odpowiedni numer: ")
             self.execute(operation)
+
 
 manager = Manager()
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', saldo=manager.saldo, warehouse=manager.warehouse)
+    products = Product.query.all()
+    saldo = manager.saldo
+    return render_template('index.html', products=products, saldo=saldo)
 
 
 @app.route('/purchase', methods=['POST'])
 def purchase():
-    product = request.form['product']
+    product_name = request.form['product']
     price = float(request.form['price'])
     quantity = int(request.form['quantity'])
 
-    if product in manager.warehouse:
-        manager.warehouse[product]['ilość'] += quantity
-        manager.saldo -= price * quantity
-        manager.history.append(f"Zakupiono {product} w ilości {quantity}")
-    else:
-        manager.warehouse[product] = {"ilość": quantity, "cena": price}
-        manager.saldo -= price * quantity
-        manager.history.append(f"Dodano nowy produkt: {product} w ilości {quantity} po cenie {price}")
+    product = Product.query.filter_by(name=product_name).first()
 
-    manager.save_data()
+    if product:
+        product.quantity += quantity
+        transaction = Transaction(operation="Zakup", product_id=product.id, quantity=quantity)
+    else:
+        product = Product(name=product_name, quantity=quantity, price=price)
+        transaction = Transaction(operation="Zakup", product=product, quantity=quantity)
+
+    db.session.add(product)
+    db.session.add(transaction)
+    db.session.commit()
+
     return redirect(url_for('index'))
 
 
 @app.route('/sale', methods=['POST'])
 def sale():
-    product = request.form['product']
+    product_name = request.form['product']
     quantity = int(request.form['quantity'])
 
-    if product in manager.warehouse:
-        if manager.warehouse[product]['ilość'] >= quantity:
-            manager.warehouse[product]['ilość'] -= quantity
-            manager.saldo += manager.warehouse[product]['cena'] * quantity
-            manager.history.append(f"Sprzedano {product} w ilości {quantity}")
-        else:
-            manager.history.append(f"Nie udało się sprzedać towaru {product}, zbyt mała ilość na magazynie")
+    product = Product.query.filter_by(name=product_name).first()
 
-    manager.save_data()
+    if product:
+        if product.quantity >= quantity:
+            product.quantity -= quantity
+            transaction = Transaction(operation="Sprzedaż", product_id=product.id, quantity=quantity)
+            db.session.add(transaction)
+            db.session.commit()
+        else:
+            manager.history.append(f"Nie udało się sprzedać towaru {product_name}, zbyt mała ilość na magazynie")
+            db.session.rollback()
+
     return redirect(url_for('index'))
 
 
 @app.route('/change_balance', methods=['POST'])
 def change_balance():
-    amount = float(request.form['amount'])
-    manager.saldo += amount
-    manager.history.append(f"Zmieniono saldo o kwotę: {amount}")
-    manager.save_data()
+    amount_str = request.form['amount']
+
+    # Upewnij się, że wartość jest niepustym łańcuchem
+    if not amount_str:
+        return redirect(url_for('index'))  # Powrót do strony głównej bez zmiany salda
+
+    try:
+        amount = float(amount_str)
+    except ValueError:
+        return redirect(url_for('index'))  # Powrót do strony głównej w przypadku błędnej wartości
+
+    if amount != 0.0:
+        manager.saldo += amount
+        # Przypisz stałą wartość product_id do transakcji zmiany salda
+        transaction = Transaction(operation="Zmiana Salda", product_id=0, quantity=amount)
+        db.session.add(transaction)
+        db.session.commit()
+
     return redirect(url_for('index'))
 
 
@@ -110,15 +127,14 @@ def change_balance():
 @app.route('/history/<int:start>/<int:end>')
 def history(start, end):
     if start is None and end is None:
-        history_data = manager.history
+        history_data = Transaction.query.all()
     else:
         history_len = len(manager.history)
         if start is None or end is None or start < 1 or end > history_len or start > end:
             return render_template('history_range.html', history_len=history_len)
-        history_data = manager.history[start - 1:end]  # Poprawione wyodrębnianie podzakresu historii
+        history_data = Transaction.query.order_by(Transaction.timestamp).slice(start - 1, end)
 
     return render_template('history.html', history_data=history_data)
-
 
 
 if __name__ == "__main__":
